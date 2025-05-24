eb9d0a1bc3dec9ad6222be610391e1af4b4d9ab2
tarekul42 <tarekulrifat205@gmail.com>
2025-05-24 21:11:19 +0600

    values added for every table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE conservation_db;
 
-CREATE TABLE rangers(
+CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(50),
     region VARCHAR(50)
 );
 
-CREATE TABLE species(
+CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
     common_name VARCHAR(50) NOT NULL,
     scientific_name VARCHAR(50) NOT NULL,
@@ -14,11 +14,110 @@ CREATE TABLE species(
     conservation_status VARCHAR(20)
 );
 
-CREATE TABLE sightings(
+CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
-    ranger_id INTEGER REFERENCES rangers(id),
-    species_id INTEGER REFERENCES species(id),
-    sighting_time DATE,
-    location VARCHAR(10),
+    ranger_id INTEGER NOT NULL REFERENCES rangers (ranger_id),
+    species_id INTEGER NOT NULL REFERENCES species (species_id),
+    sighting_time TIMESTAMP,
+    location VARCHAR(50),
     notes TEXT
-)
+);
+
+INSERT INTO
+    rangers (ranger_id, name, region)
+VALUES (
+        1,
+        'Alice Green',
+        'Northern Hills'
+    ),
+    (2, 'Bob White', 'River Delta'),
+    (
+        3,
+        'Carol King',
+        'Mountain Range'
+    );
+
+INSERT INTO
+    species (
+        species_id,
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        1,
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        2,
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        3,
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        4,
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
+
+INSERT INTO
+    sightings (
+        sighting_id,
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        4,
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    );
+
+SELECT * FROM rangers;
+
+SELECT * FROM species;
+
+SELECT * FROM sightings;
