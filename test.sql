5ba810a533471ccbf961fa82735a41a4cab27a6e
tarekul42 <tarekulrifat205@gmail.com>
2025-05-26 22:15:31 +0600

    How can you modify data using UPDATE statements?

--- test.sql
+++ test.sql
@@ -1,9 +1,3 @@
-SELECT department_id, count(\*) 
-    FROM employees 
-    GROUP BY department_id;
-
-SELECT d.department_name, avg(e.salary)
-FROM employees e
-    JOIN departments d on d.department_id = e.department_id
-GROUP BY
-    department_name;
+UPDATE employees 
+    SET emp_name = 'Rahim' 
+    WHERE emp_id = 1;
